50ba3065d5731b93ce727f445e89c57d696c85ec
Maria Izabel <izabel.162@hotmail.com>
2025-07-26 15:21:17 -0300

    inicializa repositório

---
+++ microsservicos/identidade-acesso/src/main/java/com/projetos/manutencao/identidade_acesso/IdentidadeAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.manutencao.identidade_acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentidadeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentidadeAcessoApplication.class, args);
+	}
+
+}
---
+++ microsservicos/identidade-acesso/src/test/java/com/projetos/manutencao/identidade_acesso/IdentidadeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.manutencao.identidade_acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentidadeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
