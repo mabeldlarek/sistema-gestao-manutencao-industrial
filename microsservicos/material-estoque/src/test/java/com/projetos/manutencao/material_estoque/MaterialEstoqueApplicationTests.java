a96a72e41438d55d58c0a1c9aa4a79d7554cee9d
Maria Izabel <izabel.162@hotmail.com>
2025-08-05 21:48:59 -0300

    "feat: adiciona microsservico  material-estoque"

---
+++ microsservicos/material-estoque/src/test/java/com/projetos/manutencao/material_estoque/MaterialEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.manutencao.material_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
