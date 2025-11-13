78c26314def99ffdd97e2284978cdcf15d04174b
Maria Izabel <izabel.162@hotmail.com>
2025-11-13 15:45:23 -0300

    feat: adiciona configuracao do mongo e validatiom (pom.xml)

---
+++ microsservicos/ativos/src/main/java/com/projetos/manutencao/ativos/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package com.projetos.manutencao.ativos.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(
+                "mongodb://admin:admin123@localhost:27017/identidade_db?authSource=admin"
+        );
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "identidade_db");
+    }
+}
