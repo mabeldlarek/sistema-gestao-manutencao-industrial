8fdbbc1941b234e4ec69487b619044f8fc84cfcc
Maria Izabel <izabel.162@hotmail.com>
2026-02-09 12:48:45 -0300

    fix_SecurityConfig_ajusta configuração de cors

--- microsservicos/identidade-acesso/src/main/java/com/projetos/manutencao/identidade_acesso/config/SecurityConfig.java
+++ microsservicos/identidade-acesso/src/main/java/com/projetos/manutencao/identidade_acesso/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-        .cors(Customizer.withDefaults())
+         .cors(cors -> {})
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers(
